b4d15d5ec17a48b94526853dd25878e8f1765005
madhawa-se <madhawapriyashantha@gmail.com>
2018-09-20 17:14:50 +0530

    Movie props

--- src/App.js
+++ src/App.js
@@ -8,14 +8,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-        <Movie/>
+        <Movie year="1999"  genre="Sci-Fi" title="Matrix" description="A computer hacker learns from mysterious rebels about the true nature of his reality and his role in the war aagainstits controllers"/>
       </div>
     );
   }
--- src/Movie.js
+++ src/Movie.js
@@ -3,7 +3,10 @@ import React, { Component } from 'react';
 class App extends Component {
   render() {
     return (
-      <div>Hello</div>
+      <div>
+        <h2>{this.props.year} - {this.props.genre} - {this.props.title}</h2>
+        <p>{this.props.description}</p>
+      </div>
     );
   }
 }
