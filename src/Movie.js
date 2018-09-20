42eaa0dffc70280c0761590df9d21fab9085d48a
madhawa-se <madhawapriyashantha@gmail.com>
2018-09-20 17:28:01 +0530

    All Movies

--- src/Movie.js
+++ src/Movie.js
@@ -1,11 +1,33 @@
 import React, { Component } from 'react';
 
 class App extends Component {
+
+constructor(props) {
+    super(props);
+    this.state = {
+      movies:[
+      {
+        id:1,year:1999,genre:"Sci-Fi",title:"Matrix",
+        description:"A computer hacker learns from mysterious rebels about the true nature of his reality and his role in the war aagainstits controllers"
+      },
+      {
+        id:2,year:1994,genre:"Drama",title:"The Shawshank Redemption",
+        description:"Two imprisoned men bond over a number of years"
+      }
+      ]
+    };
+  }
+
   render() {
     return (
       <div>
-        <h2>{this.props.year} - {this.props.genre} - {this.props.title}</h2>
-        <p>{this.props.description}</p>
+
+       {this.state.movies.map(
+          (movie,index)=>{
+            return  <div><h2>{movie.year} - {movie.genre} - {movie.title}</h2><p>{movie.description}</p></div>
+           } 
+        )}
+         
       </div>
     );
   }
